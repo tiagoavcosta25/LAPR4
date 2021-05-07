5ec5a8ca0e298159b8028266b3266cc879324156
Tiago Costa <1191460@isep.ipp.pt>
2021-05-07 10:02:26 +0100

    LAPR4-80 #implementation #developing TeamBuilder.java implemented

---
+++ project/base.core/src/main/java/eapli/base/teammanagement/domain/TeamBuilder.java
@@ -0,0 +1,42 @@
+package eapli.base.teammanagement.domain;
+
+import eapli.base.collaboratormanagement.domain.Collaborator;
+import eapli.framework.domain.model.DomainFactory;
+
+import java.util.Set;
+
+public class TeamBuilder implements DomainFactory<Team> {
+
+    private TeamType m_enumTeamType;
+
+    private Acronym m_oAcronym;
+
+    private TeamDescription m_oTeamDescription;
+
+    private Set<Collaborator> m_setRepresentation;
+
+    public TeamBuilder withTeamType(TeamType enumTeamType) {
+        this.m_enumTeamType = enumTeamType;
+        return this;
+    }
+
+    public TeamBuilder withAcronym(Acronym oAcronym) {
+        this.m_oAcronym = oAcronym;
+        return this;
+    }
+
+    public TeamBuilder withTeamDescription(TeamDescription oTeamDescription) {
+        this.m_oTeamDescription = oTeamDescription;
+        return this;
+    }
+
+    public TeamBuilder withRepresentation(Set<Collaborator> setRepresentation) {
+        this.m_setRepresentation = setRepresentation;
+        return this;
+    }
+
+    @Override
+    public Team build() {
+        return new Team(m_enumTeamType, m_oAcronym, m_oTeamDescription, m_setRepresentation);
+    }
+}
