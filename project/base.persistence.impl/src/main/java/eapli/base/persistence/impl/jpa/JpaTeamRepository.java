0187c6cbd58d508077bdc799fd1fd113e68c3b98
Tiago Costa <1191460@isep.ipp.pt>
2021-05-07 21:17:17 +0100

    LAPR4-80 #implementation #developing JpaTeamRepository.java and it's methods implemented

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaTeamRepository.java
@@ -0,0 +1,31 @@
+package eapli.base.persistence.impl.jpa;
+
+import eapli.base.Application;
+import eapli.base.teammanagement.domain.Team;
+import eapli.base.teammanagement.domain.TeamID;
+import eapli.base.teammanagement.repositories.TeamRepository;
+import eapli.framework.domain.repositories.TransactionalContext;
+import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class JpaTeamRepository extends JpaAutoTxRepository<Team, TeamID, TeamID>
+    implements TeamRepository {
+
+    public JpaTeamRepository(final TransactionalContext autoTx) {
+        super(autoTx, "m_oID");
+    }
+
+    public JpaTeamRepository(String puname) {
+        super(puname, Application.settings().getExtendedPersistenceProperties(), "m_oID");
+    }
+
+    @Override
+    public Optional<Team> findById(TeamID oID) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("m_oID", oID);
+        return matchOne("e.id=:m_oID", params);
+    }
+}
