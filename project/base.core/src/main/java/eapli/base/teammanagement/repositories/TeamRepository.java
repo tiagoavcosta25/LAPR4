17745a31e6147f8fb1e65d313cfb29435533a4f3
Tiago Costa <1191460@isep.ipp.pt>
2021-05-07 10:02:47 +0100

    LAPR4-80 #implementation #developing TeamRepository.java implemented

---
+++ project/base.core/src/main/java/eapli/base/teammanagement/repositories/TeamRepository.java
@@ -0,0 +1,30 @@
+package eapli.base.teammanagement.repositories;
+
+import eapli.base.teammanagement.domain.Team;
+import eapli.base.teammanagement.domain.TeamID;
+import eapli.framework.domain.repositories.DomainRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public interface TeamRepository
+        extends DomainRepository<TeamID, Team> {
+
+    /**
+     * returns the Team with the given ID
+     *
+     * @param oID
+     * @return
+     */
+    default Optional<Team> findByID(TeamID oID) {
+        return ofIdentity(oID);
+    }
+
+    public Iterable<Team> findAllActive();
+
+    List<Team> all();
+}
