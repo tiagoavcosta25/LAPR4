405d9e651e0a9f3635e51ad60f3715d10ae85297
1190967 <1190967@isep.ipp.pt>
2021-05-08 08:10:33 +0100

    LAPR4-90 #review #developing UnitaryTesting Review

--- project/base.core/src/main/java/eapli/base/servicemanagement/application/ServiceDraftSpecificationController.java
+++ project/base.core/src/main/java/eapli/base/servicemanagement/application/ServiceDraftSpecificationController.java
@@ -24,6 +24,7 @@
 package eapli.base.servicemanagement.application;
 
 import eapli.base.formmanagement.domain.*;
+import eapli.base.formmanagement.repositories.AttributeRepository;
 import eapli.base.formmanagement.repositories.FormRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.servicemanagement.domain.*;
@@ -44,6 +45,7 @@ public class ServiceDraftSpecificationController {
 
     private final ServiceDraftRepository draftRepo = PersistenceContext.repositories().serviceDrafts();
     private final FormRepository formRepo = PersistenceContext.repositories().forms();
+    private final AttributeRepository attributeRepo = PersistenceContext.repositories().attributes();
     private FormBuilder formBuilder = new FormBuilder();
     private AttributeBuilder attributeBuilder = new AttributeBuilder();
     private List<Attribute> m_lstAttributes = new ArrayList<>();
@@ -89,6 +91,7 @@ public class ServiceDraftSpecificationController {
         this.attributeBuilder = this.attributeBuilder.withDataType(strDataType);
 
         Attribute oAttribute = this.attributeBuilder.build();
+        this.attributeRepo.save(oAttribute);
         this.m_lstAttributes.add(oAttribute);
         return oAttribute;
     }
