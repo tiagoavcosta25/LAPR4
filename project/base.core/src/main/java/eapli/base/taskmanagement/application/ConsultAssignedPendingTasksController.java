961456071a8a5ce0e467c24abd0aaaabbc456b64
Tiago Costa <1191460@isep.ipp.pt>
2021-05-27 19:41:59 +0100

    LAPR4-129 #implementation #developing ConsultAssignedPendingTasksController method getTaskFilterFields implemented

--- project/base.core/src/main/java/eapli/base/taskmanagement/application/ConsultAssignedPendingTasksController.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/application/ConsultAssignedPendingTasksController.java
@@ -1,7 +1,18 @@
 package eapli.base.taskmanagement.application;
 
+import eapli.base.taskmanagement.domain.TaskFilterFields;
+import eapli.framework.application.UseCaseController;
+
 /**
+ *
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
+@UseCaseController
 public class ConsultAssignedPendingTasksController {
+
+    private final ListTaskFilterFieldsService listTaskFilterFieldsService = new ListTaskFilterFieldsService();
+
+    public Iterable<TaskFilterFields> getTaskFilterFields() {
+        return listTaskFilterFieldsService.getTaskFilterFields();
+    }
 }
