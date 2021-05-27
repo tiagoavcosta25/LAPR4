05dd7e10184ec9909d4651c186567ebcd1d89354
Tiago Costa <1191460@isep.ipp.pt>
2021-05-27 19:36:57 +0100

    LAPR4-129 #implementation #developing ListTaskFilterFieldsService service class method getTaskFilterFields implemented

--- project/base.core/src/main/java/eapli/base/taskmanagement/application/ListTaskFilterFieldsService.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/application/ListTaskFilterFieldsService.java
@@ -1,7 +1,23 @@
 package eapli.base.taskmanagement.application;
 
+import eapli.base.taskmanagement.domain.TaskFilterFields;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.application.ApplicationService;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+import java.util.Arrays;
+
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
+@ApplicationService
 public class ListTaskFilterFieldsService {
+
+    private final AuthorizationService m_oAuthz = AuthzRegistry.authorizationService();
+
+    public Iterable<TaskFilterFields> getTaskFilterFields() {
+        m_oAuthz.ensureAuthenticatedUserHasAnyOf(BaseRoles.USER);
+        return Arrays.asList(TaskFilterFields.values());
+    }
 }
