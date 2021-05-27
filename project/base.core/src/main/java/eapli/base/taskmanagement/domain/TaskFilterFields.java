eacca021d1d0d1d5defa655201c4e09bd764fa0a
Tiago Costa <1191460@isep.ipp.pt>
2021-05-27 18:10:26 +0100

    LAPR4-129 #implementation #developing TaskFilterFields and TaskOrderFields implemented

--- project/base.core/src/main/java/eapli/base/taskmanagement/domain/TaskFilterFields.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/domain/TaskFilterFields.java
@@ -3,6 +3,6 @@ package eapli.base.taskmanagement.domain;
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public enum TaskFOFields {
+public enum TaskFilterFields {
     PRIORITY, FINISHDATE, NONE;
 }
