4f1b103968953321ccf15a5169d5fcf6d08f17b8
jessicaalves1 <1190682@isep.ipp.pt>
2021-05-27 19:45:05 +0100

    LAPR4-133 #implementation #developing Ticket Classes Development

---
+++ project/base.core/src/main/java/eapli/base/servicesolicitationmanagement/domain/TicketLimitDate.java
@@ -0,0 +1,62 @@
+package eapli.base.servicesolicitationmanagement.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.time.LocalDate;
+
+@Embeddable
+public class TicketLimitDate implements ValueObject, Comparable<TicketLimitDate> {
+
+        private static final long serialVersionUID = 1L;
+
+        @Column(name = "limitDate")
+        private LocalDate m_dtLimitDate;
+
+    public TicketLimitDate(final LocalDate dtBirthDate) {
+            if (dtBirthDate.isBefore(LocalDate.now())) {
+                throw new IllegalArgumentException(
+                        "Limit Date can't be set in the past.");
+            }
+            // expression
+            this.m_dtLimitDate = dtBirthDate;
+        }
+
+    protected TicketLimitDate() {
+            // for ORM
+        }
+
+        public static TicketLimitDate valueOf(final LocalDate dtLimitDate) {
+            return new TicketLimitDate(dtLimitDate);
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof TicketLimitDate)) {
+                return false;
+            }
+
+            final TicketLimitDate that = (TicketLimitDate) o;
+            return this.m_dtLimitDate.equals(that.m_dtLimitDate);
+        }
+
+        @Override
+        public int hashCode() {
+            return this.m_dtLimitDate.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return this.m_dtLimitDate.toString();
+        }
+
+        @Override
+        public int compareTo(final TicketLimitDate arg0) {
+            return m_dtLimitDate.compareTo(arg0.m_dtLimitDate);
+        }
+
+    }
---
+++ project/base.core/src/main/java/eapli/base/servicesolicitationmanagement/domain/TicketResponse.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates and open the template
+ * in the editor.
+ */
+package eapli.base.servicesolicitationmanagement.domain;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.strings.util.StringPredicates;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * @author Jéssica Alves 1190682@isep.ipp.pt
+ */
+@Embeddable
+public class TicketResponse implements ValueObject, Comparable<TicketResponse> {
+
+    private static final long serialVersionUID = 1L;
+    private static final Integer m_intMaxLength = 40;
+
+    @Column(name = "ticketResponse")
+    private String m_strResponse;
+
+    public TicketResponse(final String strResponse) {
+        if (StringPredicates.isNullOrEmpty(strResponse) || !(strResponse.length() < m_intMaxLength)) {
+            throw new IllegalArgumentException(
+                    "Response should not be null, empty nor have more than 40 characters");
+        }
+        // expression
+        this.m_strResponse = strResponse;
+    }
+
+    protected TicketResponse() {
+        // for ORM
+    }
+
+    public static TicketResponse valueOf(final String strResponse) {
+        return new TicketResponse(strResponse);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TicketResponse)) {
+            return false;
+        }
+
+        final TicketResponse that = (TicketResponse) o;
+        return this.m_strResponse.equals(that.m_strResponse);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.m_strResponse.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.m_strResponse;
+    }
+
+    @Override
+    public int compareTo(final TicketResponse arg0) {
+        return m_strResponse.compareTo(arg0.m_strResponse);
+    }
+}
