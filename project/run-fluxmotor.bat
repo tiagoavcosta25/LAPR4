df8f433d147019e9e5e418fd72cf2c088dde69b0
Tiago Costa <1191460@isep.ipp.pt>
2021-05-29 20:25:17 +0100

    LAPR4-141 #server #developing run-fluxmotor.bat and run-fluxmotor.sh developed

---
+++ project/run-fluxmotor.bat
@@ -0,0 +1,6 @@
+REM set the class path,
+REM assumes the build was executed with maven copy-dependencies
+SET BASE_CP=base.server.fluxmotor\target\base.server.fluxmotor-1.3.0-SNAPSHOT.jar;base.server.fluxmotor\target\dependency\*;
+
+REM call the java VM, e.g, 
+java -cp %BASE_CP% net.activityflux.RunServer
