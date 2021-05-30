cc3d41ddbfb51d9e85082c0d48d4705bebfe2f94
Tiago Costa <1191460@isep.ipp.pt>
2021-05-30 19:50:20 +0100

    LAPR4-125 #implementation #development ManualTaskExecutionRepository.java implemented

---
+++ project/base.core/src/main/java/eapli/base/taskmanagement/repositories/ManualTaskExecutionRepository.java
@@ -0,0 +1,30 @@
+package eapli.base.taskmanagement.repositories;
+
+import eapli.base.activityfluxmanagement.domain.ActivityFlux;
+import eapli.base.collaboratormanagement.domain.Collaborator;
+import eapli.base.servicemanagement.domain.Service;
+import eapli.base.taskmanagement.domain.*;
+import eapli.framework.domain.repositories.DomainRepository;
+import eapli.framework.infrastructure.authz.domain.model.Username;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public interface ManualTaskExecutionRepository
+        extends DomainRepository<Long, ManualTaskExecution> {
+
+    /**
+     * returns the form with the given id
+     *
+     * @param lngID
+     * @return
+     */
+    default Optional<ManualTaskExecution> findByID(Long lngID) {
+        return ofIdentity(lngID);
+    }
+
+}
