5eae0661d53d55c27d7c013dbd5d269447ac5c66
Tiago Costa <1191460@isep.ipp.pt>
2021-05-30 21:57:09 +0100

    LAPR4-151 #implementation #developing TCPAutoTaskExecutorClient revamped

--- project/base.core/src/main/java/eapli/base/net/motorflux/TCPAutoTaskExecutorClient.java
+++ project/base.core/src/main/java/eapli/base/net/motorflux/TCPAutoTaskExecutorClient.java
@@ -1,4 +1,4 @@
-package base.server.fluxmotor.application;
+package eapli.base.net.motorflux;
 
 import eapli.base.net.SDP2021;
 import eapli.base.net.SDP2021Code;
@@ -9,15 +9,14 @@ import java.net.*;
 /**
  * @author Beatriz Seixas 1190424@isep.ipp.pt
  */
-class TCPAutoTaskExecutorClient {
+public class TCPAutoTaskExecutorClient {
     static InetAddress serverIP;
     static Socket sock;
 
     static String strIP = "127.0.0.1";
     static int intPort = 32507;
 
-    public static void main(String args[]) throws Exception {
-
+    public void run(String strPath) throws Exception {
         try { serverIP = InetAddress.getByName(strIP); }
         catch(UnknownHostException ex) {
             System.out.println("Invalid server specified: " + strIP);
@@ -33,7 +32,7 @@ class TCPAutoTaskExecutorClient {
 
         SDP2021 request = new SDP2021(SDP2021Code.AUTOTASK_REQUEST.getCode());
 
-        request.send(sOut, "D:/folder3/script3.bat");
+        request.send(sOut, strPath);
 
         sock.close();
     }
