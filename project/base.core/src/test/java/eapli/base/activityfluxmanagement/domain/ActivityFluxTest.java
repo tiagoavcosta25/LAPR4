561aee62a688a3c36b0f6d81931475997c110e3b
1190967 <1190967@isep.ipp.pt>
2021-05-30 06:24:40 +0100

    LAPR4-134 #review #fixing Unitary Testing (Activity Flux)

--- project/base.core/src/test/java/eapli/base/activityfluxmanagement/domain/ActivityFluxTest.java
+++ project/base.core/src/test/java/eapli/base/activityfluxmanagement/domain/ActivityFluxTest.java
@@ -1,4 +1,60 @@
-import junit.framework.TestCase;
-public class ActivityFluxTest extends TestCase {
-  
+package eapli.base.activityfluxmanagement.domain;
+
+import eapli.base.formmanagement.domain.*;
+import eapli.base.taskmanagement.domain.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+public class ActivityFluxTest {
+
+    @Test
+    public void ensureCanBuildActivityFluxWithApprovalAndManualTask() {
+        final Form oApprovalForm = new Form(FormName.valueOf("Approval Form"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Approval"),
+                AttributeLabel.valueOf("Approval"), AttributeDescription.valueOf("Approval|Rejected"), AttributeRegex.valueOf("[0-9]+"),
+                AttributeScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final ManualTask oApprovalTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, oApprovalForm);
+        final Form oForm = new Form(FormName.valueOf("Form Name"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Attribute"),
+                AttributeLabel.valueOf("Label"), AttributeDescription.valueOf("Description"), AttributeRegex.valueOf("[0-9]+"), AttributeScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final ManualTask oManualTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.LOW, oForm);
+        final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oApprovalTask, oManualTask)));
+        assertTrue(subject != null);
+    }
+
+    @Test
+    public void ensureCanBuildActivityFluxWithOnlyManualTask() {
+        final Form oForm = new Form(FormName.valueOf("Form Name"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Attribute"),
+                AttributeLabel.valueOf("Label"), AttributeDescription.valueOf("Description"), AttributeRegex.valueOf("[0-9]+"), AttributeScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final ManualTask oManualTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.LOW, oForm);
+        final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oManualTask)));
+        assertTrue(subject != null);
+    }
+
+    @Test
+    public void ensureCanBuildActivityFluxWithApprovalAndAutoTask() {
+        final Form oApprovalForm = new Form(FormName.valueOf("Approval Form"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Approval"),
+                AttributeLabel.valueOf("Approval"), AttributeDescription.valueOf("Approval|Rejected"), AttributeRegex.valueOf("[0-9]+"),
+                AttributeScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final ManualTask oApprovalTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, oApprovalForm);
+        final AutomaticTask oAutoTask = new AutomaticTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, AutomaticTaskScript.valueOf("D:/folder3/script3.bat"));
+        final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oApprovalTask, oAutoTask)));
+        assertTrue(subject != null);
+    }
+
+    @Test
+    public void ensureCanBuildActivityFluxWithOnlyAutoTask() {
+        final AutomaticTask oAutoTask = new AutomaticTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, AutomaticTaskScript.valueOf("D:/folder3/script3.bat"));
+        final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oAutoTask)));
+        assertTrue(subject != null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureCantBuildActivityFluxWithoutTasks() {
+        final ActivityFlux subject = new ActivityFlux(new ArrayList<>());
+        assertTrue(subject != null);
+    }
+
 }
