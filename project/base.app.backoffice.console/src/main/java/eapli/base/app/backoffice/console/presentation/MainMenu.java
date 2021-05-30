5813f348c1d09532502126ac9d98a7038060c1e9
1190967 <1190967@isep.ipp.pt>
2021-05-30 08:03:00 +0100

    LAPR4-134 #review #fixing Main Menu (Changes)

--- project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/MainMenu.java
+++ project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/MainMenu.java
@@ -27,7 +27,6 @@ import eapli.base.app.backoffice.console.presentation.catalogue.*;
 import eapli.base.app.backoffice.console.presentation.collaborator.*;
 import eapli.base.app.backoffice.console.presentation.service.*;
 import eapli.base.app.backoffice.console.presentation.task.AssignTasksAction;
-import eapli.base.app.backoffice.console.presentation.ticket.ServiceSolicitationAction;
 import eapli.base.app.backoffice.console.presentation.teamtype.TeamTypeRegisterAction;
 import eapli.base.app.backoffice.console.presentation.team.CreateTeamAction;
 import eapli.base.app.backoffice.console.presentation.team.ListTeamTypeAction;
@@ -79,7 +78,6 @@ public class MainMenu extends AbstractUI {
     private static final int SERVICES_ADD_FORM = 4;
     private static final int SERVICES_ADD_TASK = 5;
     private static final int SERVICES_SAVE = 6;
-    private static final int SERVICES_SOLICITATION = 7;
 
     // CATALOGUE
     private static final int CATALOGUE_SPECIFY = 1;
@@ -232,7 +230,6 @@ public class MainMenu extends AbstractUI {
         menu.addItem(SERVICES_ADD_FORM, "Add/Update Service Draft (Form)", new ServiceDraftFormSpecificationAction());
         menu.addItem(SERVICES_ADD_TASK, "Add/Update Service Draft (Task)", new ServiceDraftTaskSpecificationAction());
         menu.addItem(SERVICES_SAVE, "Save Service", new SaveDraftAction());
-        menu.addItem(SERVICES_SOLICITATION, "Save Solicitation", new ServiceSolicitationAction());
         menu.addItem(EXIT_OPTION, RETURN_LABEL, Actions.SUCCESS);
 
         return menu;
