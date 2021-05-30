b44aca9928ea08520396e66860369a0901c87e00
1190967 <1190967@isep.ipp.pt>
2021-05-30 06:22:57 +0100

    LAPR4-137 #javascript #developing Javascript Changes

--- project/base.app.user.console/src/main/java/eapli/base/app/user/console/net/dashboard/www/dashboard.js
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/net/dashboard/www/dashboard.js
@@ -5,19 +5,19 @@ function refreshPendingTasks() {
         request.onload = function() {
             vBoard.innerHTML = this.responseText;
             vBoard.style.color="black";
-            setTimeout(pendingTasks, 2000);
+            setTimeout(refreshPendingTasks, 2000);
             };
             
         request.ontimeout = function() {
             vBoard.innerHTML = "Server timeout, still trying ...";
             vBoard.style.color="red";
-            setTimeout(pendingTasks, 100);
+            setTimeout(refreshPendingTasks, 100);
         };
         
         request.onerror = function() { 
             vBoard.innerHTML = "No server reply, still trying ...";
             vBoard.style.color="red";
-            setTimeout(pendingTasks, 5000);
+            setTimeout(refreshPendingTasks, 5000);
         };
         
   	request.open("GET", "/dashboard_info", true);
