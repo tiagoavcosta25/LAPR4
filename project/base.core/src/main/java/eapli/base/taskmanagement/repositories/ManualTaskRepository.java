f80fa3998f7feb21a328e770c4006032b5550b70
Tiago Costa <1191460@isep.ipp.pt>
2021-05-30 19:51:34 +0100

    LAPR4-125 #implementation #development ManualTaskRepository.java fixed

--- project/base.core/src/main/java/eapli/base/taskmanagement/repositories/ManualTaskRepository.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/repositories/ManualTaskRepository.java
@@ -1,21 +1,15 @@
 package eapli.base.taskmanagement.repositories;
 
-import eapli.base.activityfluxmanagement.domain.ActivityFlux;
-import eapli.base.collaboratormanagement.domain.Collaborator;
-import eapli.base.servicemanagement.domain.Service;
-import eapli.base.taskmanagement.domain.*;
+import eapli.base.taskmanagement.domain.ManualTask;
+import eapli.base.taskmanagement.domain.ManualTaskExecution;
 import eapli.framework.domain.repositories.DomainRepository;
-import eapli.framework.infrastructure.authz.domain.model.Username;
 
 import java.util.Optional;
 
 /**
- *
- * @author Pedro Santos 1190967@isep.ipp.pt
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public interface ManualTaskRepository
-        extends DomainRepository<Long, ManualTaskExecution> {
+public interface ManualTaskRepository extends DomainRepository<Long, ManualTask> {
 
     /**
      * returns the form with the given id
@@ -23,7 +17,7 @@ public interface ManualTaskRepository
      * @param lngID
      * @return
      */
-    default Optional<ManualTaskExecution> findByID(Long lngID) {
+    default Optional<ManualTask> findByID(Long lngID) {
         return ofIdentity(lngID);
     }
 
