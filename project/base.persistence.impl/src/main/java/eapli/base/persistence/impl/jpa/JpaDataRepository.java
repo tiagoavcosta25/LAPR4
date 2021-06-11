329a02e68a5b98747662b1551d509f04f23a467b
Tiago Costa <1191460@isep.ipp.pt>
2021-06-11 23:27:10 +0100

    LAPR4-129 #implementation #development [US-2-3021] JpaDataRepository methods implemented

--- project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaDataRepository.java
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaDataRepository.java
@@ -99,6 +99,17 @@ class JpaDataRepository extends HelpDeskJpaRepositoryBase<Ticket, Long, Long>
 
     @Override
     public Long numberOfHighPriorityActivities(String oUserName) {
-       return null;
+        final TypedQuery<Long> q = entityManager().createQuery(
+                "Select count(mte) from ActivityFluxExecution a join a.m_lstFlux lst " +
+                        "inner join Ticket t on t.m_oFluxExecution.id = a.id " +
+                        "inner join TaskExecution te on te.id = lst.id " +
+                        "inner join Task ts on ts.id = te.m_oTask.id " +
+                        "inner join ManualTaskExecution mte on mte.id = te.id " +
+                        "where mte.m_oCollaborator.m_oSystemUser.username.value =: un and te.m_oTaskStatus = 'PENDING' " +
+                        "and ts.m_oTaskPriority =: highPrio",
+                Long.class);
+        q.setParameter("un", oUserName);
+        q.setParameter("highPrio", TaskPriority.HIGH);
+        return q.getSingleResult();
     }
 }
