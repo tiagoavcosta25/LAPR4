16e9a0aa983c4a05aa1b5d885c7afa8bc8b3f963
Tiago Costa <1191460@isep.ipp.pt>
2021-06-13 20:16:21 +0100

    LAPR4-189 #implementation #developing [US-0-1004] ValidateScript class implemented

---
+++ project/base.core/src/main/java/eapli/base/grammar/ValidateScript.java
@@ -0,0 +1,45 @@
+package eapli.base.grammar;
+
+import eapli.base.grammar.antlr.autotask.AutoTaskLexer;
+import eapli.base.grammar.antlr.autotask.AutoTaskParser;
+import eapli.base.grammar.antlr.validateform.ValidateFormLexer;
+import eapli.base.grammar.antlr.validateform.ValidateFormParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class ValidateScript {
+    public static boolean validateForm(String strPath) throws IOException {
+        ValidateFormLexer lexer = new ValidateFormLexer(CharStreams.fromFileName(strPath));
+        CommonTokenStream tokens= new CommonTokenStream(lexer);
+        ValidateFormParser parser = new ValidateFormParser(tokens);
+        parser.removeErrorListeners();
+        SyntaxError syntaxError = new SyntaxError();
+        parser.addErrorListener(syntaxError);
+        parser.start();
+
+        if(syntaxError.isError()){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateAutoTask(String strPath) throws IOException {
+        AutoTaskLexer lexer = new AutoTaskLexer(CharStreams.fromFileName(strPath));
+        CommonTokenStream tokens= new CommonTokenStream(lexer);
+        AutoTaskParser parser = new AutoTaskParser(tokens);
+        parser.removeErrorListeners();
+        SyntaxError syntaxError = new SyntaxError();
+        parser.addErrorListener(syntaxError);
+        parser.start();
+
+        if(syntaxError.isError()){
+            return false;
+        }
+        return true;
+    }
+}
