993b169b8d8924f51a9292f540f162b0cb747abc
1190424 <1190424@isep.ipp.pt>
2021-06-13 20:10:22 +0100

    LAPR4-188 #grammar #developing [US-0-1004]

---
+++ project/base.core/src/main/java/eapli/base/grammar/SyntaxError.java
@@ -0,0 +1,25 @@
+package eapli.base.grammar;
+
+import org.antlr.v4.runtime.*;
+
+public class SyntaxError extends BaseErrorListener {
+    private boolean error;
+
+    public SyntaxError(){
+        super();
+        error = false;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg,
+                            RecognitionException e)
+    {
+        this.error = true;
+    }
+    public boolean isError() {
+        return this.error;
+    }
+}
