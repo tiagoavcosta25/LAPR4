44058af7dda194268794d55678913d1293d9919f
Tiago Costa <1191460@isep.ipp.pt>
2021-06-13 16:27:34 +0100

    LAPR4-129 #implementation #fix [US-2-3021] InMemoryDataRepository.java method fix

--- project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryDataRepository.java
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryDataRepository.java
@@ -52,7 +52,20 @@ public class InMemoryDataRepository extends InMemoryDomainRepository<Ticket, Lon
 
     @Override
     public Long numberOfNearExpiredActivities(String oUserName) {
-        return prioritySearch(oUserName, TaskPriority.LOW);
+        Long counter = 0L;
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime in1h = now.plusHours(1);
+        Iterable<Ticket> lst = match(ticket -> ticket.executionFlux().flux().stream()
+                .allMatch(taskExecution -> taskExecution.status().equals(TaskExecutionStatus.PENDING))
+                && ticket.limitDate().getM_dtLimitDate().isAfter(now)
+                && ticket.limitDate().getM_dtLimitDate().isBefore(in1h));
+        for(Ticket t : lst) {
+            for(TaskExecution te : t.executionFlux().flux()) {
+                ManualTaskExecution me = (ManualTaskExecution) te;
+                if (me.getM_oCollaborator().user().username().toString().equals(oUserName)) counter++;
+            }
+        }
+        return counter;
     }
 
     @Override
@@ -62,7 +75,7 @@ public class InMemoryDataRepository extends InMemoryDomainRepository<Ticket, Lon
 
     @Override
     public Long numberOfMediumPriorityActivities(String oUserName) {
-        return 0L;
+       return 0L;
     }
 
     @Override
@@ -78,7 +91,7 @@ public class InMemoryDataRepository extends InMemoryDomainRepository<Ticket, Lon
             for(TaskExecution te : t.executionFlux().flux()) {
                 ManualTaskExecution me = (ManualTaskExecution) te;
                 if (me.getM_oCollaborator().user().username().toString().equals(oUserName)
-                        && me.task().priority().equals(tp)) counter++;
+                        && me.task().priority().equals(tp) counter++;
             }
         }
         return counter;
