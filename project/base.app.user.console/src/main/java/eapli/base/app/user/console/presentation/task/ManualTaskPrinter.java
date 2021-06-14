a804d5e321319eb1e4e76800a2098dfc4eb07d1d
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 06:02:29 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] ManualTaskPrinter.java class fixed

--- project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/ManualTaskPrinter.java
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/ManualTaskPrinter.java
@@ -1,15 +1,16 @@
 package eapli.base.app.user.console.presentation.task;
 
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
 import eapli.base.taskmanagement.specification.domain.ManualTask;
 import eapli.framework.visitor.Visitor;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public class ManualTaskPrinter implements Visitor<ManualTask> {
+public class ManualTaskPrinter implements Visitor<ManualTaskExecution> {
 
     @Override
-    public void visit(ManualTask visitee) {
+    public void visit(ManualTaskExecution visitee) {
         System.out.printf("Manual Task %s", visitee.toString());
     }
 }
