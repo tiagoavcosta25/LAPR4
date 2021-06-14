7b70a65f4e4172e791738d6c84173ee09545b29e
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 06:00:05 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] ExecuteAutomaticTaskUI.java class fixed

--- project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/ExecuteAutomaticTaskUI.java
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/ExecuteAutomaticTaskUI.java
@@ -1,8 +1,10 @@
 package eapli.base.app.user.console.presentation.task;
 
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
 import eapli.base.activityfluxmanagement.specification.domain.ActivityFlux;
 import eapli.base.servicemanagement.domain.Service;
 import eapli.base.taskmanagement.execution.application.ExecuteAutomaticTaskController;
+import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
 import eapli.base.taskmanagement.specification.domain.AutomaticTask;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
@@ -11,59 +13,6 @@ import eapli.framework.presentation.console.SelectWidget;
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public class ExecuteAutomaticTaskUI extends AbstractUI {
-    private final ExecuteAutomaticTaskController theController = new ExecuteAutomaticTaskController();
+public class ExecuteAutomaticTaskUI  {
 
-    @Override
-    protected boolean doShow() {
-        try {
-
-            final ActivityFlux af = selectActivityFlux();
-
-            final AutomaticTask autoTask = selectAutomaticTask(af);
-            int id = Math.toIntExact(autoTask.identity());
-            System.out.println();
-            String strOp = Console.readLine("Confirm the execution of task with code " + id + "? (Y/N) ");
-
-            if(strOp.compareToIgnoreCase("Y") == 0){
-                this.theController.executeTask(autoTask);
-                System.out.printf("Operation Successful. The Following Automatic Task was executed successfully > id:" +
-                        " %s\n\n", id);
-            } else{
-                System.out.println("Operation Cancelled.");
-            }
-        } catch (final Exception ex) {
-            System.out.println("Error while executing an Automatic Task. " + ex.getMessage());
-        }
-        return false;
-    }
-
-    private AutomaticTask selectAutomaticTask(ActivityFlux af) {
-        System.out.println();
-        System.out.println("List of Pending Automatic Tasks - Select an Automatic Task");
-        final Iterable<AutomaticTask> listTasks = theController.getPendingTasks(af);
-        if(!listTasks.iterator().hasNext())
-            throw new IllegalArgumentException("No Pending Tasks avaiable!");
-        final SelectWidget<AutomaticTask> selectorAutomaticTask = new SelectWidget<>("Select an Automatic Task", listTasks,
-                new AutomaticTaskPrinter());
-        selectorAutomaticTask.show();
-        return selectorAutomaticTask.selectedElement();
-    }
-
-    private ActivityFlux selectActivityFlux() {
-        System.out.println();
-        System.out.println("List of Activity Flux - Select an Activity Flux");
-        final Iterable<Service> listActivityFlux = theController.getActivityFlow();
-        if(!listActivityFlux.iterator().hasNext())
-            throw new IllegalArgumentException("No Pending Tasks avaiable!");
-        final SelectWidget<Service> selectorActivityFlux = new SelectWidget<>("Select an Activity Flow", listActivityFlux,
-                new ActivityFluxPrinter());
-        selectorActivityFlux.show();
-        return selectorActivityFlux.selectedElement().flux();
-    }
-
-    @Override
-    public String headline() {
-        return "Execute Manual Task";
-    }
 }
