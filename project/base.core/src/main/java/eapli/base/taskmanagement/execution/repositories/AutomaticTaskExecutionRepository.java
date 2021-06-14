c3d347d7c42a8806315a56bc8fc2eb829f87ca25
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:43:58 +0100

    LAPR4-129 #implementation #developing [US-2-3021] AutomaticTaskExecutionRepository.java class implemented

---
+++ project/base.core/src/main/java/eapli/base/taskmanagement/execution/repositories/AutomaticTaskExecutionRepository.java
@@ -0,0 +1,11 @@
+package eapli.base.taskmanagement.execution.repositories;
+
+import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
+import eapli.framework.domain.repositories.DomainRepository;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public interface AutomaticTaskExecutionRepository extends DomainRepository<Long, AutomaticTaskExecution> {
+
+}
