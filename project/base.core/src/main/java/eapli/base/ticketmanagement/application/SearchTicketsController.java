141adfa552b407b058186084001766646ad16d45
1190682 <1190682@isep.ipp.pt>
2021-06-14 19:50:51 +0100

    LAPR4-182 #implementation #developing [US-2-3031] Ticket Search Controller Development

---
+++ project/base.core/src/main/java/eapli/base/ticketmanagement/application/SearchTicketsController.java
@@ -0,0 +1,30 @@
+package eapli.base.ticketmanagement.application;
+
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.ticketmanagement.domain.Ticket;
+import eapli.base.ticketmanagement.repository.TicketRepository;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+import java.util.List;
+
+/**
+ * @author Jéssica Alves 1190682@isep.ipp.pt
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public class SearchTicketsController {
+
+    private final AuthorizationService m_oAuthz = AuthzRegistry.authorizationService();
+    private final TicketRepository m_oTicketRepo = PersistenceContext.repositories().tickets();
+
+    public List<Ticket> getTicketHistory(){
+        this.m_oAuthz.ensureAuthenticatedUserHasAnyOf(BaseRoles.POWER_USER, BaseRoles.ADMIN, BaseRoles.HS_MANAGER, BaseRoles.COLLABORATOR);
+        return this.m_oTicketRepo.getTicketHistory(this.m_oAuthz.session().get().authenticatedUser());
+    }
+
+    public List<Ticket> getOnGoingTickets(){
+        this.m_oAuthz.ensureAuthenticatedUserHasAnyOf(BaseRoles.POWER_USER, BaseRoles.ADMIN, BaseRoles.HS_MANAGER, BaseRoles.COLLABORATOR);
+        return this.m_oTicketRepo.getOnGoingTickets(this.m_oAuthz.session().get().authenticatedUser());
+    }
+}
