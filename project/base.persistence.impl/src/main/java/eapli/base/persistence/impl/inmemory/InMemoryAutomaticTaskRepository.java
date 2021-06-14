4e286948b246e00cf17d014c70cfa1ad5483a365
1190967 <1190967@isep.ipp.pt>
2021-06-14 16:24:26 +0100

    LAPR4-176 #implementation #developing [US-0-3003] Ticket Refinement

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryAutomaticTaskRepository.java
@@ -0,0 +1,23 @@
+package eapli.base.persistence.impl.inmemory;
+
+import eapli.base.taskmanagement.specification.domain.AutomaticTask;
+import eapli.base.taskmanagement.specification.repositories.AutomaticTaskRepository;
+import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public class InMemoryAutomaticTaskRepository extends InMemoryDomainRepository<AutomaticTask, Long>
+        implements AutomaticTaskRepository {
+
+    static {
+        InMemoryInitializer.init();
+    }
+
+    @Override
+    public Optional<AutomaticTask> findById(Long number) {
+        return matchOne(automaticTask -> automaticTask.id().equals(number));
+    }
+}
