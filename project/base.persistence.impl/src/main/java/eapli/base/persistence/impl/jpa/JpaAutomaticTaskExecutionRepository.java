c4f2f9ecb5fcfbe8f4f6ff1a6f31ba347805256d
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:54:21 +0100

    LAPR4-129 #implementation #developing [US-2-3021] JpaAutomaticTaskExecutionRepository.java class implemented

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaAutomaticTaskExecutionRepository.java
@@ -0,0 +1,16 @@
+package eapli.base.persistence.impl.jpa;
+
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
+import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecutionStatus;
+import eapli.base.taskmanagement.execution.repositories.AutomaticTaskExecutionRepository;
+
+import javax.persistence.TypedQuery;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class JpaAutomaticTaskExecutionRepository {
+
+
+}
