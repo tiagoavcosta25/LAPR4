aa032516bc04828fcd6d414bc7f33a44b55c70e4
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:47:58 +0100

    LAPR4-129 #implementation #developing [US-2-3021] InMemoryAutomaticTaskExecutionRepository.java class implemented

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryAutomaticTaskExecutionRepository.java
@@ -0,0 +1,21 @@
+package eapli.base.persistence.impl.inmemory;
+
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
+import eapli.base.activityfluxmanagement.execution.repositories.ActivityFluxExecutionRepository;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.repositories.AutomaticTaskExecutionRepository;
+import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class InMemoryAutomaticTaskExecutionRepository  {
+
+
+}
