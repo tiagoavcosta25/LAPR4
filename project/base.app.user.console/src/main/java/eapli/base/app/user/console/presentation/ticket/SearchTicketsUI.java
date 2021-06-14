cb9900e590c5c74aeed3d61242bc836b8b7e6a66
1190682 <1190682@isep.ipp.pt>
2021-06-14 19:50:33 +0100

    LAPR4-182 #implementation #developing [US-2-3031] Ticket Search UI Development

---
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/ticket/SearchTicketsUI.java
@@ -0,0 +1,78 @@
+package eapli.base.app.user.console.presentation.ticket;
+
+import eapli.base.cataloguemanagement.domain.Catalogue;
+import eapli.base.formmanagement.domain.Attribute;
+import eapli.base.formmanagement.domain.Form;
+import eapli.base.servicemanagement.domain.Service;
+import eapli.base.ticketmanagement.application.SearchTicketsController;
+import eapli.base.ticketmanagement.application.ServiceSolicitationController;
+import eapli.base.ticketmanagement.domain.Ticket;
+import eapli.base.ticketmanagement.domain.TicketUrgency;
+import eapli.framework.io.util.Console;
+import eapli.framework.presentation.console.AbstractUI;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jéssica Alves 1190682@isep.ipp.pt
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public class SearchTicketsUI extends AbstractUI {
+
+    private final SearchTicketsController theController = new SearchTicketsController();
+
+    @Override
+    protected boolean doShow() {
+        try {
+            List<Ticket> lstTickets;
+
+            String strOp = Console.readLine("Choose the mode of search:\n\n [1] On going\n [2] History\n >");
+            if (strOp.compareToIgnoreCase("1") == 0) {
+                lstTickets = this.theController.getOnGoingTickets();
+            } else {
+                lstTickets = this.theController.getTicketHistory();
+            }
+
+            do {
+                Ticket oTicket = chooseOne(lstTickets, "Choose a Ticket", "Ticket");
+                oTicket.toString();
+                strOp = Console.readLine("Do you want to see a detailed view of some more tickets? (Y/N) >");
+            } while (strOp.compareToIgnoreCase("Y") == 0);
+
+        } catch(Exception e){
+            System.out.println("Error in searching a Ticket.");
+        }
+        return false;
+    }
+
+    public static <T> T chooseOne(Iterable<T> itElements, String strHeader, String strElementName) {
+        try{
+            Integer i = 1;
+            List<T> lstTemp = new ArrayList<>();
+            System.out.printf("\n==========================================\n%s\n==========================================\n\n", strHeader);
+            for(T t : itElements){
+                System.out.printf("[%d] %s\n", i, t.toString());
+                i++;
+                lstTemp.add(t);
+            }
+            if(lstTemp.isEmpty()){
+                System.out.println("There is no " + strElementName + "s in the Database.\n\n");
+                return null;
+            }
+            Integer intOp = Integer.parseInt(Console.readLine("\n\n\nSelect " + strElementName + " Number >"));
+
+            return lstTemp.get(intOp - 1);
+
+        } catch (Exception e){
+            System.out.println("Error in selecting.\n\n");
+            return null;
+        }
+    }
+
+    @Override
+    public String headline() {
+        return "Search Tickets";
+    }
+}
