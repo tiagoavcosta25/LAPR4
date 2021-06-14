95ec89fe5c71ba51925dc8f1cfc989f7eeaff88f
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 06:10:42 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] AutomaticTaskRepository.java methods fixed

--- project/base.core/src/main/java/eapli/base/taskmanagement/specification/repositories/AutomaticTaskRepository.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/specification/repositories/AutomaticTaskRepository.java
@@ -1,15 +1,22 @@
 package eapli.base.taskmanagement.specification.repositories;
 
-import eapli.base.servicemanagement.domain.Service;
 import eapli.base.taskmanagement.specification.domain.AutomaticTask;
 import eapli.framework.domain.repositories.DomainRepository;
+
+import java.util.Optional;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
 public interface AutomaticTaskRepository extends DomainRepository<Long, AutomaticTask> {
 
-    public Iterable<AutomaticTask> getPendingAutomaticTasks(Long idFlux);
-
-    public Iterable<Service> getActivityFlux();
+    /**
+     * returns the form with the given id
+     *
+     * @param lngID
+     * @return
+     */
+    default Optional<AutomaticTask> findByID(Long lngID) {
+        return ofIdentity(lngID);
+    }
 }
