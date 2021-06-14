719b55d54cbdd0b5e64c3feec443449fe6c32aff
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:52:25 +0100

    LAPR4-129 #implementation #developing [US-2-3021] InMemoryManualTaskExecutionRepository.java method implemented

--- project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryManualTaskExecutionRepository.java
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryManualTaskExecutionRepository.java
@@ -1,8 +1,29 @@
 package eapli.base.persistence.impl.inmemory;
 
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
+import eapli.base.activityfluxmanagement.execution.repositories.ActivityFluxExecutionRepository;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecutionStatus;
+import eapli.base.taskmanagement.execution.repositories.ManualTaskExecutionRepository;
+import eapli.framework.infrastructure.authz.domain.model.Username;
+import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public class InMemoryManualTaskExecutionRepository{
+public class InMemoryManualTaskExecutionRepository extends InMemoryDomainRepository<ManualTaskExecution, Long>
+        implements ManualTaskExecutionRepository {
+
+    static {
+        InMemoryInitializer.init();
+    }
+
 
 }
