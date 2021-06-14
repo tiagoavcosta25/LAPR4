697bf068f71f24354b3562f7805e57b7fddf5a05
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:55:48 +0100

    LAPR4-129 #implementation #developing [US-2-3021] JpaManualTaskExecutionRepository.java class implemented

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaManualTaskExecutionRepository.java
@@ -0,0 +1,17 @@
+package eapli.base.persistence.impl.jpa;
+
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecutionStatus;
+import eapli.base.taskmanagement.execution.repositories.ManualTaskExecutionRepository;
+import eapli.framework.infrastructure.authz.domain.model.Username;
+
+import javax.persistence.TypedQuery;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class JpaManualTaskExecutionRepository {
+
+
+}
