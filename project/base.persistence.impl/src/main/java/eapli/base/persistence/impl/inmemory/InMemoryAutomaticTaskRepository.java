c4dc06fb9401c00db2184744885a6bb52c7b363f
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:50:56 +0100

    LAPR4-129 #implementation #developing [US-2-3021] InMemoryAutomaticTaskRepository.java method implemented

--- project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryAutomaticTaskRepository.java
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryAutomaticTaskRepository.java
@@ -1,8 +1,20 @@
 package eapli.base.persistence.impl.inmemory;
 
+import eapli.base.taskmanagement.specification.domain.AutomaticTask;
+import eapli.base.taskmanagement.specification.domain.Task;
+import eapli.base.taskmanagement.specification.repositories.AutomaticTaskRepository;
+import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
+
+import java.util.Optional;
+
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public class InMemoryAutomaticTaskRepository  {
+public class InMemoryAutomaticTaskRepository extends InMemoryDomainRepository<AutomaticTask, Long>
+        implements AutomaticTaskRepository {
+
+    static {
+        InMemoryInitializer.init();
+    }
 
 }
