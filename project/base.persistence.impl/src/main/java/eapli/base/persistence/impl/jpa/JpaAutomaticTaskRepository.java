504136c5d2474a9d56a607763b3ec7bcb812b366
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 06:12:29 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] JpaAutomaticTaskRepository.java methods fixed

--- project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaAutomaticTaskRepository.java
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaAutomaticTaskRepository.java
@@ -1,10 +1,11 @@
 package eapli.base.persistence.impl.jpa;
 
-import eapli.base.servicemanagement.domain.Service;
 import eapli.base.taskmanagement.specification.domain.AutomaticTask;
 import eapli.base.taskmanagement.specification.repositories.AutomaticTaskRepository;
 
-import javax.persistence.TypedQuery;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
@@ -17,26 +18,9 @@ public class JpaAutomaticTaskRepository extends HelpDeskJpaRepositoryBase<Automa
     }
 
     @Override
-    public Iterable<AutomaticTask> getPendingAutomaticTasks(Long idFlux) {
-        final TypedQuery<AutomaticTask> q = entityManager().createQuery(
-                "Select distinct at from ActivityFlux af join af.m_lstFlux lst " +
-                        "inner join Task t on t.id = lst.id " +
-                        "inner join AutomaticTask at on at.id = t.id " +
-                        "where t.m_oTaskStatus = 'PENDING' and af.id =:idflux",
-                AutomaticTask.class);
-        q.setParameter("idflux", idFlux);
-        return q.getResultList();
-    }
-
-    @Override
-    public Iterable<Service> getActivityFlux() {
-        final TypedQuery<Service> q = entityManager().createQuery(
-                "Select distinct s from ActivityFlux af join af.m_lstFlux lst " +
-                        "inner join Service s on s.m_oActivityFlux.id = af.id " +
-                        "inner join AutomaticTask at on at.id = lst.id " +
-                        "inner join Task t on t.id = at.id " +
-                        "where t.m_oTaskStatus = 'PENDING'",
-                Service.class);
-        return q.getResultList();
+    public Optional<AutomaticTask> findById(Long lngID) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("taskID", lngID);
+        return matchOne("e.id=:taskID", params);
     }
 }
