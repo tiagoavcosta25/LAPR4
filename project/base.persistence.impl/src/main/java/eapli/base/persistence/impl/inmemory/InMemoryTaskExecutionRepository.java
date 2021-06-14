67a2f10bf958f239c3d57fb3ba35b5e3daaed822
1190682 <1190682@isep.ipp.pt>
2021-06-14 16:31:10 +0100

    LAPR4-184 #implementation #developing [US-2-3031]  Activity Flux Adjustments

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryTaskExecutionRepository.java
@@ -0,0 +1,24 @@
+package eapli.base.persistence.impl.inmemory;
+
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.repositories.TaskExecutionRepository;
+import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Jéssica Alves 1190682@isep.ipp.pt
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public class InMemoryTaskExecutionRepository extends InMemoryDomainRepository<TaskExecution, Long>
+        implements TaskExecutionRepository {
+
+    static {
+        InMemoryInitializer.init();
+    }
+
+    @Override
+    public Optional<TaskExecution> findById(Long number) {
+        return matchOne(taskExecution -> taskExecution.id().equals(number));
+    }
+}
