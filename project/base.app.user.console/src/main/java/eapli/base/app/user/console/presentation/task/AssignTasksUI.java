ed962dc0255046376dc414ec2d6d484457f5444e
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:58:19 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] AssignTasksUI.java class fixed

--- project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/AssignTasksUI.java
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/AssignTasksUI.java
@@ -2,6 +2,7 @@ package eapli.base.app.user.console.presentation.task;
 
 import eapli.base.app.user.console.utils.PrintList;
 import eapli.base.taskmanagement.execution.application.AssignTasksController;
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
 import eapli.base.taskmanagement.specification.domain.ManualTask;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
@@ -11,29 +12,5 @@ import eapli.framework.presentation.console.AbstractUI;
  */
 public class AssignTasksUI extends AbstractUI {
 
-    private final AssignTasksController theController = new AssignTasksController();
 
-    @Override
-    protected boolean doShow() {
-        try {
-
-            Iterable<ManualTask> itTasks = this.theController.getPendingTasks();
-            String op;
-            do{
-                ManualTask oManualTask = PrintList.chooseOne(itTasks, "Choose a Task", "Task");
-                theController.assignTask(oManualTask);
-                op = Console.readLine("Do you want to assign more task (Y/N) >");
-            } while(op.compareToIgnoreCase("y") == 0);
-
-        } catch(Exception e){
-            System.out.println("Error in assigning a Task.");
-        }
-
-        return false;
-    }
-
-    @Override
-    public String headline() {
-        return "Assign Tasks";
-    }
 }
