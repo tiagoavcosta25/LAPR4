0663ed3e671b8965cb0f5a1452559660a34e62c6
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:59:23 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] AutomaticTaskPrinter.java method fixed

--- project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/AutomaticTaskPrinter.java
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/AutomaticTaskPrinter.java
@@ -1,15 +1,16 @@
 package eapli.base.app.user.console.presentation.task;
 
+import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
 import eapli.base.taskmanagement.specification.domain.AutomaticTask;
 import eapli.framework.visitor.Visitor;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public class AutomaticTaskPrinter implements Visitor<AutomaticTask> {
+public class AutomaticTaskPrinter implements Visitor<AutomaticTaskExecution> {
 
     @Override
-    public void visit(AutomaticTask visitee) {
+    public void visit(AutomaticTaskExecution visitee) {
         System.out.printf("Automatic Task %s", visitee.toString());
     }
 }
