e38c801c6e51e5d4df3f2f83dadc7b2d4e4a3aca
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 02:26:39 +0100

    LAPR4-129 #implementation #developing [US-2-3021] FileUtils.java class created

---
+++ project/base.core/src/main/java/eapli/base/util/FileUtils.java
@@ -0,0 +1,14 @@
+package eapli.base.util;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class FileUtils {
+
+}
