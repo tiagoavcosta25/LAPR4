1c46c6bf4786c21e6eaf01e530c8480d280077a2
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:46:30 +0100

    LAPR4-129 #implementation #developing [US-2-3021] ManualTaskExecutionRepository.java class implemented

---
+++ project/base.core/src/main/java/eapli/base/taskmanagement/execution/repositories/ManualTaskExecutionRepository.java
@@ -0,0 +1,11 @@
+package eapli.base.taskmanagement.execution.repositories;
+
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.framework.domain.repositories.DomainRepository;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public interface ManualTaskExecutionRepository extends DomainRepository<Long, ManualTaskExecution> {
+
+}
