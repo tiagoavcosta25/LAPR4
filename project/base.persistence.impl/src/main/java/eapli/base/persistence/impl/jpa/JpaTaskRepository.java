2db1521f787337466b6fd0683d13b28209fe1152
1190967 <1190967@isep.ipp.pt>
2021-06-14 16:16:56 +0100

    LAPR4-176 #implementation #developing [US-0-3003] Task Changes

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaTaskRepository.java
@@ -0,0 +1,33 @@
+package eapli.base.persistence.impl.jpa;
+
+import eapli.base.taskmanagement.specification.domain.Task;
+import eapli.base.taskmanagement.specification.repositories.TaskRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ *
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+class JpaTaskRepository
+        extends HelpDeskJpaRepositoryBase<Task, Long, Long>
+        implements TaskRepository {
+
+    public JpaTaskRepository() {
+        super("m_lngID");
+    }
+
+    @Override
+    public Optional<Task> findById(Long lngID) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("taskID", lngID);
+        return matchOne("e.id=:taskID", params);
+    }
+
+    @Override
+    public boolean isManualTask(Long lngID) {
+        return findByID(lngID).isPresent();
+    }
+}
