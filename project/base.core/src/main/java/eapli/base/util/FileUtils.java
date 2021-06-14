057ca87d306d3a26ba383e336e43dabc88441d04
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 02:30:12 +0100

    LAPR4-129 #implementation #developing [US-2-3021] FileUtils.java method implemented

--- project/base.core/src/main/java/eapli/base/util/FileUtils.java
+++ project/base.core/src/main/java/eapli/base/util/FileUtils.java
@@ -1,14 +1,15 @@
 package eapli.base.util;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
 public class FileUtils {
-
+    public static boolean verifyExtension(String filename, String extension) {
+        Optional<String> ext = Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+        return ext.map(s -> s.equals(extension)).orElse(false);
+    }
 }
