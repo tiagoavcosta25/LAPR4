679b68e3712015b774a81468f696d1dede90f65c
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:46:49 +0100

    LAPR4-129 #implementation #developing [US-2-3021] ManualTaskExecutionRepository.java method implemented

--- project/base.core/src/main/java/eapli/base/taskmanagement/execution/repositories/ManualTaskExecutionRepository.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/execution/repositories/ManualTaskExecutionRepository.java
@@ -1,11 +1,14 @@
 package eapli.base.taskmanagement.execution.repositories;
 
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
 import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
 import eapli.framework.domain.repositories.DomainRepository;
+import eapli.framework.infrastructure.authz.domain.model.Username;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
 public interface ManualTaskExecutionRepository extends DomainRepository<Long, ManualTaskExecution> {
 
+    Iterable<ManualTaskExecution> getHisPendingManualTasks(Username oUsername);
 }
