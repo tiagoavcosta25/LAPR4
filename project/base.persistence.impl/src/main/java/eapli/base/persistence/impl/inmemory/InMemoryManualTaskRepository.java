4464a39e3b8475a50d8b5338e18c7543c096de46
1190967 <1190967@isep.ipp.pt>
2021-06-14 16:25:37 +0100

    LAPR4-176 #implementation #developing [US-0-3003] Manual Task Adjustments

---
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryManualTaskRepository.java
@@ -0,0 +1,23 @@
+package eapli.base.persistence.impl.inmemory;
+
+import eapli.base.taskmanagement.specification.domain.ManualTask;
+import eapli.base.taskmanagement.specification.repositories.ManualTaskRepository;
+import eapli.framework.infrastructure.repositories.impl.inmemory.InMemoryDomainRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public class InMemoryManualTaskRepository extends InMemoryDomainRepository<ManualTask, Long>
+        implements ManualTaskRepository {
+
+    static {
+        InMemoryInitializer.init();
+    }
+
+    @Override
+    public Optional<ManualTask> findById(Long number) {
+        return Optional.of(data().get(number));
+    }
+}
