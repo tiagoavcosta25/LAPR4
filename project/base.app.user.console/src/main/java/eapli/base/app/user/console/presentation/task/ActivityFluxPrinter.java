6e1d1f8b137d315b444b30db1e49e2487c897c76
Tiago Costa <1191460@isep.ipp.pt>
2021-06-14 05:57:14 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] ActivityFluxPrinter.java class fixed

--- project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/ActivityFluxPrinter.java
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/ActivityFluxPrinter.java
@@ -1,14 +1,12 @@
 package eapli.base.app.user.console.presentation.task;
 
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
 import eapli.base.servicemanagement.domain.Service;
 import eapli.framework.visitor.Visitor;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
-public class ActivityFluxPrinter implements Visitor<Service> {
-    @Override
-    public void visit(Service visitee) {
-        System.out.println("Activity Flux " +  visitee.briefDescription().toString());
-    }
+public class ActivityFluxPrinter  {
+
 }
