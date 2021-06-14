4c711915bf6404b0a87e4a46a44aa82030ffc700
1190967 <1190967@isep.ipp.pt>
2021-06-14 16:31:02 +0100

    LAPR4-176 #implementation #developing [US-0-3003] AutoTask

---
+++ project/base.core/src/main/java/eapli/base/taskmanagement/specification/repositories/AutomaticTaskRepository.java
@@ -0,0 +1,22 @@
+package eapli.base.taskmanagement.specification.repositories;
+
+import eapli.base.taskmanagement.specification.domain.AutomaticTask;
+import eapli.framework.domain.repositories.DomainRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public interface AutomaticTaskRepository extends DomainRepository<Long, AutomaticTask> {
+
+    /**
+     * returns the form with the given id
+     *
+     * @param lngID
+     * @return
+     */
+    default Optional<AutomaticTask> findByID(Long lngID) {
+        return ofIdentity(lngID);
+    }
+}
