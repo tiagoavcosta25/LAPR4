61844c550be6f4aea0baf1e0e82f744c34e20cfd
1190682 <1190682@isep.ipp.pt>
2021-06-14 19:49:29 +0100

    LAPR4-182 #implementation #developing [US-2-3031] Ticket Repository Changes

--- project/base.core/src/main/java/eapli/base/ticketmanagement/repository/TicketRepository.java
+++ project/base.core/src/main/java/eapli/base/ticketmanagement/repository/TicketRepository.java
@@ -2,7 +2,9 @@ package eapli.base.ticketmanagement.repository;
 
 import eapli.base.ticketmanagement.domain.Ticket;
 import eapli.framework.domain.repositories.DomainRepository;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TicketRepository
@@ -17,4 +19,8 @@ public interface TicketRepository
     default Optional<Ticket> findByID(Long lngID) {
         return ofIdentity(lngID);
     }
+
+    List<Ticket> getTicketHistory(final SystemUser oUser);
+
+    List<Ticket> getOnGoingTickets(final SystemUser oUser);
 }
