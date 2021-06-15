8eb9bad8d56d6ef4a3426ceb52a4407c2587949b
1190967 <1190967@isep.ipp.pt>
2021-06-15 18:07:07 +0100

    LAPR4-193 #listener #developing [US-0-1007] Listener Mandatory Attribute Script Function Execution

--- project/base.core/src/main/java/eapli/base/grammar/antlr/validateform/FormListener.java
+++ project/base.core/src/main/java/eapli/base/grammar/antlr/validateform/FormListener.java
@@ -2,6 +2,8 @@ package eapli.base.grammar.antlr.validateform;
 
 import eapli.base.ticketmanagement.domain.Response;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -9,7 +11,7 @@ import java.util.Stack;
  */
 public class FormListener extends ValidateFormBaseListener {
 
-    private final Stack<Boolean> stack = new Stack<>();
+    private final Stack<String> stack = new Stack<>();
     private Response m_oResponse;
 
     public FormListener(Response oResponse) {
@@ -17,7 +19,32 @@ public class FormListener extends ValidateFormBaseListener {
     }
 
     public Boolean getResult() {
-        return stack.peek();
+        return Boolean.valueOf(stack.peek());
+    }
+
+    @Override
+    public void exitExecMandatory(ValidateFormParser.ExecMandatoryContext ctx) {
+        String str = this.stack.pop();
+
+        List<Integer> lstAttributes = new ArrayList<>();
+
+        while(!str.isEmpty()){
+            if(str.indexOf(",") == -1){
+                lstAttributes.add(Integer.parseInt(str.trim()));
+                break;
+            }
+            lstAttributes.add(Integer.parseInt(str.substring(0, str.indexOf(",")).trim()));
+            str = str.substring(str.indexOf(",") + 1);
+        }
+
+        for(Integer i : lstAttributes){
+            if(this.m_oResponse.getResponses().get(i - 1).isEmpty()){
+                this.stack.push("false");
+                break;
+            }
+        }
+
+        this.stack.push("true");
     }
 
 }
