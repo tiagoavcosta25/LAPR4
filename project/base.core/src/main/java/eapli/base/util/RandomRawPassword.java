dba464454f7c22a688381128f42ad9de7ae3fa12
Tiago Costa <1191460@isep.ipp.pt>
2021-06-15 21:04:17 +0100

    LAPR4-129 #implementation #developing [US-2-3021] RandomRawPassword class created

---
+++ project/base.core/src/main/java/eapli/base/util/RandomRawPassword.java
@@ -0,0 +1,9 @@
+package eapli.base.util;
+
+import eapli.base.usermanagement.domain.BasePasswordPolicy;
+import eapli.framework.infrastructure.authz.application.PasswordPolicy;
+import eapli.framework.strings.util.Strings;
+
+public class RandomRawPassword {
+
+}
