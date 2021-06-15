dff78133677e7d293c26542bd425f561e4a15e9a
Tiago Costa <1191460@isep.ipp.pt>
2021-06-15 21:04:33 +0100

    LAPR4-129 #implementation #developing [US-2-3021] RandomRawPassword methods implemented

--- project/base.core/src/main/java/eapli/base/util/RandomRawPassword.java
+++ project/base.core/src/main/java/eapli/base/util/RandomRawPassword.java
@@ -5,5 +5,13 @@ import eapli.framework.infrastructure.authz.application.PasswordPolicy;
 import eapli.framework.strings.util.Strings;
 
 public class RandomRawPassword {
+    private static final int DEFAULT_LENGTH = 12;
+    private static final String LOWER_CHARS = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER_CHARS = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+    private static final String NUMBERS = "0123456789";
+    private static final String SPECIAL = "_-+*/=#?!.&$€";
+    public static final String ALLOWED_CHARS;
+    private final String rawPassword;
+
 
 }
