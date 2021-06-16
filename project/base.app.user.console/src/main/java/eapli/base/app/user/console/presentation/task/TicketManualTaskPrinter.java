0fb6d34aac444d53ca49da17750328450772d6ca
Tiago Costa <1191460@isep.ipp.pt>
2021-06-16 08:23:47 +0100

    LAPR4-129 #implementation #developing [US-2-3021] TicketManualTaskPrinter.java class implemented

---
+++ project/base.app.user.console/src/main/java/eapli/base/app/user/console/presentation/task/TicketManualTaskPrinter.java
@@ -0,0 +1,16 @@
+package eapli.base.app.user.console.presentation.task;
+
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.ticketmanagement.domain.Ticket;
+import eapli.framework.visitor.Visitor;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class TicketManualTaskPrinter implements Visitor<Ticket> {
+
+
+
+
+}
