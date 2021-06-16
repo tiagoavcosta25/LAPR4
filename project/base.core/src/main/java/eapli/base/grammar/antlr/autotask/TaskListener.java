73595a1f4605540b1e1e61ab573bda63d7e21674
Tiago Costa <1191460@isep.ipp.pt>
2021-06-16 06:27:25 +0100

    LAPR4-195 #listener #developing [US-0-1007] TaskListener.java creation

---
+++ project/base.core/src/main/java/eapli/base/grammar/antlr/autotask/TaskListener.java
@@ -0,0 +1,8 @@
+package eapli.base.grammar.antlr.autotask;
+
+/**
+ * @author Tiago Costa 1191460@isep.ipp.pt
+ */
+public class TaskListener extends AutoTaskBaseListener{
+    //TODO: Finish Listener
+}
