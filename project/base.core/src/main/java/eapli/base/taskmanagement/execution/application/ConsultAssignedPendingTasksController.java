3c7b3818bbef6ef57a1fb0d95e812232274777d7
Tiago Costa <1191460@isep.ipp.pt>
2021-06-16 08:26:00 +0100

    LAPR4-129 #implementation #fixing [US-2-3021] ConsultAssignedPendingTasksController method purged

--- project/base.core/src/main/java/eapli/base/taskmanagement/execution/application/ConsultAssignedPendingTasksController.java
+++ project/base.core/src/main/java/eapli/base/taskmanagement/execution/application/ConsultAssignedPendingTasksController.java
@@ -1,14 +1,27 @@
 package eapli.base.taskmanagement.execution.application;
 
 import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.repositories.ManualTaskExecutionRepository;
 import eapli.base.taskmanagement.specification.domain.ManualTask;
 import eapli.base.taskmanagement.specification.domain.TaskFilterFields;
 import eapli.base.taskmanagement.specification.domain.TaskOrderFields;
 import eapli.base.taskmanagement.specification.repositories.TaskRepository;
+import eapli.base.ticketmanagement.domain.Ticket;
+import eapli.base.ticketmanagement.domain.TicketUrgency;
+import eapli.base.ticketmanagement.repository.TicketRepository;
 import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.framework.application.UseCaseController;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -18,25 +31,33 @@ import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 public class ConsultAssignedPendingTasksController {
 
     private final AuthorizationService m_oAuthz = AuthzRegistry.authorizationService();
-    private final TaskRepository m_oTaskRepo = PersistenceContext.repositories().tasks();
+    private final ManualTaskExecutionRepository mTaskExecRep = PersistenceContext.repositories().manualTaskExec();
+    private final TicketRepository mTicketRep = PersistenceContext.repositories().tickets();
     private final ListTaskFilterFieldsService listTaskFilterFieldsService = new ListTaskFilterFieldsService();
     private final ListTaskOrderFieldsService listTaskOrderFieldsService = new ListTaskOrderFieldsService();
 
+    // TODO: Finish Class
+
     public Iterable<TaskFilterFields> getTaskFilterFields() {
         return listTaskFilterFieldsService.getTaskFilterFields();
+    }
+
+    public Iterable<TicketUrgency> getPriorityFilters() {
+        m_oAuthz.ensureAuthenticatedUserHasAnyOf(BaseRoles.COLLABORATOR);
+        return Arrays.asList(TicketUrgency.values());
     }
 
     public Iterable<TaskOrderFields> getTaskOrderFields() {
         return listTaskOrderFieldsService.getTaskOrderFields();
     }
 
-    public Iterable<ManualTask> getTasksOfCollaborator() {
+    public Iterable<Ticket> getTasksOfCollaborator() {
         m_oAuthz.ensureAuthenticatedUserHasAnyOf(BaseRoles.COLLABORATOR);
-        return null;
+        return mTicketRep.getPendingManualTasksByTicket(m_oAuthz.session().get().authenticatedUser().username());
     }
 
-    public Iterable<ManualTask> getTasksOfCollaborator(TaskFilterFields filterBy) {
-return null;
+    public Iterable<Ticket> getTasksOfCollaborator(TaskFilterFields filterBy, String value) {
+
     }
 
     public Iterable<ManualTask> getTasksOfCollaborator(TaskOrderFields orderBy) {
@@ -44,6 +65,6 @@ return null;
     }
 
     public Iterable<ManualTask> getTasksOfCollaborator(TaskFilterFields filterBy, TaskOrderFields orderBy) {
-return null;
+        return null;
     }
 }
