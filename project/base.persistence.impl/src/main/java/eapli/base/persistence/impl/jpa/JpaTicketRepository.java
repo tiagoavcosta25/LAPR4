2a531d81ade61084e1781761aaa2f6aa27160536
Tiago Costa <1191460@isep.ipp.pt>
2021-06-17 05:14:30 +0100

    LAPR4-129 #implementation #developing [US-2-3021] JpaTicketRepository.java method getPendingManualTasksByTicketOrdered implemented

--- project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaTicketRepository.java
+++ project/base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaTicketRepository.java
@@ -1,24 +1,20 @@
 package eapli.base.persistence.impl.jpa;
 
-import eapli.base.cataloguemanagement.domain.Catalogue;
-import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
 import eapli.base.taskmanagement.execution.domain.TaskExecutionStatus;
+import eapli.base.taskmanagement.specification.domain.TaskOrderFields;
 import eapli.base.ticketmanagement.domain.Ticket;
 import eapli.base.ticketmanagement.domain.TicketStatus;
+import eapli.base.ticketmanagement.domain.TicketUrgency;
 import eapli.base.ticketmanagement.repository.TicketRepository;
-import eapli.base.util.Application;
-import eapli.framework.domain.repositories.TransactionalContext;
 import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 import eapli.framework.infrastructure.authz.domain.model.Username;
-import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
 
 import javax.persistence.TypedQuery;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
+ * @author Tiago Costa 1191460@isep.ipp.pt
  * @author Jéssica Alves 1190682@isep.ipp.pt
  * @author Pedro Santos 1190967@isep.ipp.pt
  */
@@ -73,4 +69,47 @@ class JpaTicketRepository
         q.setParameter("uname", oUsername);
         return q.getResultList();
     }
+
+    @Override
+    public Iterable<Ticket> getPendingManualTasksByTicketOrdered(Username oUsername, TaskOrderFields orderField) {
+        Comparator<Ticket> SortByPriority = (a, b) -> {
+            if(a.urgency().equals(TicketUrgency.LOW)) {
+                if(b.urgency().equals(TicketUrgency.LOW)) return 0;
+                else return -1;
+            }
+            else if(a.urgency().equals(TicketUrgency.MEDIUM)) {
+                if(b.urgency().equals(TicketUrgency.LOW)) return 1;
+                else if(b.urgency().equals(TicketUrgency.MEDIUM)) return 0;
+                else return -1;
+            } else {
+                if(b.urgency().equals(TicketUrgency.HIGH)) return 0;
+                else return 1;
+            }
+        };
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT DISTINCT t FROM ActivityFluxExecution afe JOIN afe.m_lstFlux lst ")
+                .append("INNER JOIN Ticket t ON t.m_oFluxExecution = afe.id ")
+                .append("INNER JOIN ManualTaskExecution mte ON mte.id = lst.id ")
+                .append("INNER JOIN TaskExecution te ON te.id = mte.id ")
+                .append("WHERE mte.m_oCollaborator.m_oSystemUser.username =: uname AND te.m_oTaskStatus =: pending ")
+                .append("AND afe.m_oProgress = mte.id ");
+        switch(orderField) {
+            case FINISHDATE:
+                query.append("ORDER BY t.m_oLimitDate");
+                break;
+            case REVERSEFINISHDATE:
+                query.append("ORDER BY t.m_oLimitDate DESC");
+                break;
+            case PRIORITY:
+                return null;
+            case REVERSEPRIORITY:
+                return null;
+        }
+        final TypedQuery<Ticket> q = entityManager().createQuery(query.toString(), Ticket.class);
+        q.setParameter("pending", TaskExecutionStatus.PENDING);
+        q.setParameter("uname", oUsername);
+        return q.getResultList();
+    }
+
+
 }
