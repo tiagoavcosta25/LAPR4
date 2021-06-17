b2a6323f21f1e573deccef3a37f8ea48295f45c0
Tiago Costa <1191460@isep.ipp.pt>
2021-06-17 05:01:00 +0100

    LAPR4-129 #implementation #developing [US-2-3021] CatalogueSearchTitleUI.java LOGGER implemented

--- project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/catalogue/CatalogueSearchTitleUI.java
+++ project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/catalogue/CatalogueSearchTitleUI.java
@@ -3,11 +3,15 @@ package eapli.base.app.backoffice.console.presentation.catalogue;
 import eapli.base.cataloguemanagement.domain.CatalogueTitle;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author Tiago Costa 1191460@isep.ipp.pt
  */
 public class CatalogueSearchTitleUI extends AbstractUI {
+
+    private static final Logger LOGGER = LogManager.getLogger(CatalogueSearchTitleUI.class);
 
     @Override
     protected boolean doShow() {
@@ -16,7 +20,7 @@ public class CatalogueSearchTitleUI extends AbstractUI {
             ListCatalogueUI listCatalogueUI = new ListCatalogueUI(CatalogueTitle.valueOf(title), false);
             listCatalogueUI.show();
         } catch (final Exception ex) {
-            System.out.println("Error while listing catalogues.");
+            LOGGER.error("Error while listing catalogues.");
         }
         return false;
     }
