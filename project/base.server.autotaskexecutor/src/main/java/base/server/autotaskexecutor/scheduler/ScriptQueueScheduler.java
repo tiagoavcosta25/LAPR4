8bf55451a8c36193dd8f8d1bbddc53636a1eb605
1190967 <1190967@isep.ipp.pt>
2021-06-18 04:34:46 +0100

    LAPR4-197 #implementation #developing [US-3-4071] Script Queue Scheduler Syncronized Passive Waiting

--- project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/scheduler/ScriptQueueScheduler.java
+++ project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/scheduler/ScriptQueueScheduler.java
@@ -5,6 +5,7 @@ import eapli.base.taskmanagement.specification.domain.AutomaticTaskScript;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
@@ -13,33 +14,46 @@ import java.util.List;
  */
 
 public class ScriptQueueScheduler {
+
     private List<LinkedList<AutomaticTaskScript>> m_lstQueues;
+    private List<ReentrantLock> m_lstLocks;
 
     public ScriptQueueScheduler(Integer intNumberHandlers) {
         this.m_lstQueues = new ArrayList<>();
+        this.m_lstLocks = new ArrayList<>();
 
         for(int i = 0; i < intNumberHandlers; i++){
             this.m_lstQueues.add(new LinkedList<>());
+            this.m_lstLocks.add(new ReentrantLock());
         }
     }
 
-    public synchronized void addScript(AutomaticTaskScript oScript) {
-        int i = 0, intSize = Integer.MAX_VALUE, intThread = -1;
+    public void addScript(AutomaticTaskScript oScript) {
+        int i = 0, intSize = Integer.MAX_VALUE, intThreadNum = -1;
         for(LinkedList<AutomaticTaskScript> lst : m_lstQueues){
             if(lst.size() < intSize){
                 intSize = lst.size();
-                intThread = i;
+                intThreadNum = i;
             }
             i++;
         }
-        this.m_lstQueues.get(intThread).add(oScript);
+
+        this.m_lstQueues.get(intThreadNum).add(oScript);
+
+        synchronized (this.m_lstLocks.get(intThreadNum)){
+            this.m_lstLocks.get(intThreadNum).notify();
+        }
     }
 
     public synchronized LinkedList<AutomaticTaskScript> getQueue(Integer intThreadNum) { return this.m_lstQueues.get(intThreadNum); }
 
     public synchronized Integer getSize(Integer intThreadNum) { return this.m_lstQueues.get(intThreadNum).size(); }
 
-    public synchronized AutomaticTaskScript getScriptForExecution(Integer intThreadNum) {
+    public AutomaticTaskScript getScriptForExecution(Integer intThreadNum) throws InterruptedException {
+        synchronized (this.m_lstLocks.get(intThreadNum)){
+            this.m_lstLocks.get(intThreadNum).wait();
+        }
+
         AutomaticTaskScript oScript = this.m_lstQueues.get(intThreadNum).getFirst();
         this.m_lstQueues.get(intThreadNum).remove(oScript);
         return oScript;
