964961603d034416b1e30b21edfc08f59c1dfdce
1190682 <1190682@isep.ipp.pt>
2021-06-18 12:59:07 +0100

    LAPR4-198 #implementation #developing [US-3-4071] Script Handler Development

--- project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/fcfs/ScriptHandlerFCFS.java
+++ project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/fcfs/ScriptHandlerFCFS.java
@@ -1,4 +1,16 @@
 package base.server.autotaskexecutor.fcfs;
+import base.server.autotaskexecutor.scheduler.ScriptHandlerScheduler;
+import base.server.autotaskexecutor.scheduler.ScriptQueueScheduler;
+import eapli.base.grammar.ScriptAlgorithms;
+import eapli.base.grammar.ScriptMode;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecutionResult;
+import eapli.base.taskmanagement.execution.repositories.TaskExecutionRepository;
+import eapli.base.taskmanagement.specification.domain.AutomaticTaskScript;
+import javafx.util.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
@@ -7,6 +19,9 @@ package base.server.autotaskexecutor.fcfs;
  */
 
 public class ScriptHandlerFCFS extends Thread {
+
+    private static final Logger LOGGER = LogManager.getLogger(ScriptHandlerFCFS.class);
+    private final TaskExecutionRepository m_oTaskExecRepo = PersistenceContext.repositories().taskExecs();
 
     private ScriptQueueFCFS m_oQueue;
     private Integer m_intThreadNumber;
@@ -18,14 +33,29 @@ public class ScriptHandlerFCFS extends Thread {
 
     public void run(){
         while(true){
-            while (this.m_oQueue.getSize() < 1){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            LOGGER.trace("Executing Automatic Task...");
+
+            try {
+                Pair<Long, AutomaticTaskScript> oPair = this.m_oQueue.firstComeFirstServed();
+
+                Boolean blnFLag = ScriptAlgorithms.executeAutoTask(oPair.getValue().toString(), ScriptMode.LISTENER);
+
+                TaskExecution oTask = this.m_oTaskExecRepo.findByID(oPair.getKey()).get();
+                oTask.setExecuted();
+
+                if(blnFLag){
+                    oTask.setResult(TaskExecutionResult.SUCCESS);
+                    LOGGER.trace("Automatic Task Result: Success.");
+                    ;               } else{
+                    oTask.setResult(TaskExecutionResult.ERROR);
+                    LOGGER.trace("Automatic Task Result: Error.");
                 }
+
+                this.m_oTaskExecRepo.save(oTask);
+
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
             }
-            System.out.printf("Thread %d: %s\n", this.m_intThreadNumber, this.m_oQueue.firstComeFirstServed().toString());
         }
     }
 }
