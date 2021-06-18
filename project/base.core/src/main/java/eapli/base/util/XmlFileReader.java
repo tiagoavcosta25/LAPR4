974a900653541b54ad4276c7e4aa6dfdcdda325c
Tiago Costa <1191460@isep.ipp.pt>
2021-06-18 15:35:20 +0100

    LAPR4-195 #implementation #listener [US-0-1007] XmlFileReader class implemented

---
+++ project/base.core/src/main/java/eapli/base/util/XmlFileReader.java
@@ -0,0 +1,21 @@
+package eapli.base.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class XmlFileReader {
+
+
+
+}
