d6b4e09ee1d95f7771bcb8cbb6513132a60cd5c5
1190967 <1190967@isep.ipp.pt>
2021-06-18 03:48:48 +0100

    LAPR4-197 #implementation #developing [US-3-4071] Scheduler Taker Adjustments to Implement Server Support and Receive Messages in the SDP2021 Protocol

--- project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/scheduler/ScriptTakerScheduler.java
+++ project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/scheduler/ScriptTakerScheduler.java
@@ -1,6 +1,15 @@
 package base.server.autotaskexecutor.scheduler;
 
+import eapli.base.net.SDP2021;
+import eapli.base.net.SDP2021Code;
 import eapli.base.taskmanagement.specification.domain.AutomaticTaskScript;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /**
  *
@@ -10,15 +19,51 @@ import eapli.base.taskmanagement.specification.domain.AutomaticTaskScript;
 
 public class ScriptTakerScheduler extends Thread {
 
-    private ScriptQueueScheduler m_oQueue;
-    private AutomaticTaskScript m_oScript;
+    private static final Logger LOGGER = LogManager.getLogger(ScriptTakerScheduler.class);
 
-    public ScriptTakerScheduler(ScriptQueueScheduler oQueue, String strScript) {
+    private Socket m_oSocket;
+    private ScriptQueueScheduler m_oQueue;
+
+    public ScriptTakerScheduler(Socket oSocket, ScriptQueueScheduler oQueue) {
+        this.m_oSocket = oSocket;
         this.m_oQueue = oQueue;
-        this.m_oScript = AutomaticTaskScript.valueOf(strScript);
     }
 
     public void run(){
-        this.m_oQueue.addScript(m_oScript);
+        InetAddress clientIP;
+
+        clientIP = m_oSocket.getInetAddress();
+        LOGGER.trace("New client connection from " + clientIP.getHostAddress() +
+                ", port number " + m_oSocket.getPort());
+
+        try(DataOutputStream sOut = new DataOutputStream(m_oSocket.getOutputStream());
+            DataInputStream sIn = new DataInputStream(m_oSocket.getInputStream())){
+
+            SDP2021 request;
+            SDP2021 response;
+
+            while((request = new SDP2021(sIn)).getCode() != SDP2021Code.END.getCode()) {
+
+                LOGGER.trace("Adding Automatic Task to Executing Queue...");
+
+                this.m_oQueue.addScript(AutomaticTaskScript.valueOf(request.getData()));
+            }
+            LOGGER.trace("Asked to close");
+            response = new SDP2021(SDP2021Code.ROGER.getCode());
+            response.send(sOut, "Goodbye");
+            LOGGER.trace("Client " + clientIP.getHostAddress() + ", port number: " + m_oSocket.getPort() +
+                    " disconnected");
+            m_oSocket.close();
+        } catch (IOException e) {
+            //e.printStackTrace();
+        } finally {
+            try {
+                LOGGER.trace("Client " + clientIP.getHostAddress() + ", port number: " + m_oSocket.getPort() +
+                        " disconnected");
+                m_oSocket.close();
+            } catch (final IOException e) {
+                LOGGER.error("While closing the client socket", e);
+            }
+        }
     }
 }
