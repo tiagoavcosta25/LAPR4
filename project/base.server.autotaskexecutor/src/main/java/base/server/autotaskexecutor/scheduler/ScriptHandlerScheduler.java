f94bc88a5ea8ff9dd5b61fa44e51ffffbaac220b
1190967 <1190967@isep.ipp.pt>
2021-06-18 05:16:08 +0100

    LAPR4-197 #implementation #developing [US-3-4071] Handler Changes to Better Support the Identification of the Task, Update its Result and Status And Act Accordingly

--- project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/scheduler/ScriptHandlerScheduler.java
+++ project/base.server.autotaskexecutor/src/main/java/base/server/autotaskexecutor/scheduler/ScriptHandlerScheduler.java
@@ -2,6 +2,12 @@ package base.server.autotaskexecutor.scheduler;
 
 import eapli.base.grammar.ScriptAlgorithms;
 import eapli.base.grammar.ScriptMode;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecutionResult;
+import eapli.base.taskmanagement.execution.repositories.TaskExecutionRepository;
+import eapli.base.taskmanagement.specification.domain.AutomaticTaskScript;
+import javafx.util.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,6 +20,7 @@ import org.apache.logging.log4j.Logger;
 public class ScriptHandlerScheduler extends Thread {
 
     private static final Logger LOGGER = LogManager.getLogger(ScriptHandlerScheduler.class);
+    private final TaskExecutionRepository m_oTaskExecRepo = PersistenceContext.repositories().taskExecs();
 
     private ScriptQueueScheduler m_oQueue;
     private Integer m_intThreadNumber;
@@ -28,8 +35,25 @@ public class ScriptHandlerScheduler extends Thread {
             LOGGER.trace("Executing Automatic Task...");
 
             try {
-                ScriptAlgorithms.executeAutoTask(this.m_oQueue.getScriptForExecution(this.m_intThreadNumber).toString(), ScriptMode.LISTENER);
+                Pair<Long, AutomaticTaskScript> oPair = this.m_oQueue.getScriptForExecution(this.m_intThreadNumber);
+
+                Boolean blnFLag = ScriptAlgorithms.executeAutoTask(oPair.getValue().toString(), ScriptMode.LISTENER);
+
+                TaskExecution oTask = this.m_oTaskExecRepo.findByID(oPair.getKey()).get();
+                oTask.setExecuted();
+
+                if(blnFLag){
+                    oTask.setResult(TaskExecutionResult.SUCCESS);
+                    LOGGER.trace("Automatic Task Result: Success.");
+;               } else{
+                    oTask.setResult(TaskExecutionResult.ERROR);
+                    LOGGER.trace("Automatic Task Result: Error.");
+                }
+
+                this.m_oTaskExecRepo.save(oTask);
+
             } catch (InterruptedException e) {
+                //e.printStackTrace();
             }
         }
     }
