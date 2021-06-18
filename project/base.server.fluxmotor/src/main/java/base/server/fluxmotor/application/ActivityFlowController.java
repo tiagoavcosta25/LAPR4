1b547fb1ec71b904798ade67cbc0f5dc5d56a558
Tiago Costa <1191460@isep.ipp.pt>
2021-06-18 16:32:09 +0100

    LAPR4-195 #implementation #listener [US-0-1007] ActivityFlowController update

--- project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/application/ActivityFlowController.java
+++ project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/application/ActivityFlowController.java
@@ -3,6 +3,13 @@ package base.server.fluxmotor.application;
 import eapli.base.activityfluxmanagement.execution.domain.ActivityFluxExecution;
 import eapli.base.activityfluxmanagement.execution.repositories.ActivityFluxExecutionRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.net.SDP2021;
+import eapli.base.net.SDP2021Code;
+import eapli.base.net.motorflux.ActivityFlowClient;
+import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
+import eapli.base.taskmanagement.execution.domain.TaskExecution;
+import eapli.base.taskmanagement.execution.repositories.TaskExecutionRepository;
+import eapli.base.util.Application;
 import eapli.framework.application.UseCaseController;
 
 import java.util.Optional;
@@ -13,7 +20,9 @@ import java.util.Optional;
 @UseCaseController
 public class ActivityFlowController {
 
+    private static final String EXECUTE_SERVER_IP = Application.settings().getExecuteServerIp();
     private final ActivityFluxExecutionRepository m_oAFERepo = PersistenceContext.repositories().fluxExecs();
+    private final TaskExecutionRepository m_oTERepo = PersistenceContext.repositories().taskExecs();
     private final FluxDataService m_oFluxDataService = new FluxDataService();
 
     public String prepareFluxData(String oUserName) {
@@ -24,6 +33,29 @@ public class ActivityFlowController {
         Optional<ActivityFluxExecution> afe = m_oAFERepo.findByID(fluxID);
         if(afe.isPresent()) {
             afe.get().advanceProgress();
+
+            Long idTask = afe.get().currentProgress().currentProgress();
+
+            if(idTask != -1) {
+                Optional<TaskExecution> oTe = m_oTERepo.findByID(idTask);
+                TaskExecution te;
+                if(!oTe.isPresent()) return "FAILURE";
+                te = oTe.get();
+                if(te.getClass().equals(AutomaticTaskExecution.class)) {
+                    AutomaticTaskExecution ate = (AutomaticTaskExecution) te;
+                    ate.setExecuting();
+                    ActivityFlowClient afc = new ActivityFlowClient(EXECUTE_SERVER_IP);
+                    afc.retrieveInformation(te.id() + "," +
+                            ate.getM_oAutomaticTask().script().toString(), SDP2021Code.AUTOTASK_REQUEST.getCode());
+                    afc.retrieveInformation("", SDP2021Code.END.getCode());
+                } else {
+
+                }
+            }
+
+
+
+
             return "SUCCESS";
         }
         return "FAILURE";
