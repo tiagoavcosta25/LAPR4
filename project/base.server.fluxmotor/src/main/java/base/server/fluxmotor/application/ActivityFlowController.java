d98ed1d469e19c8b33fe18b853ed6979f03303f8
Tiago Costa <1191460@isep.ipp.pt>
2021-06-19 07:25:12 +0100

    LAPR4-195 #implementation #listener [US-0-1007] ActivityFlowController methods implemented

--- project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/application/ActivityFlowController.java
+++ project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/application/ActivityFlowController.java
@@ -7,6 +7,7 @@ import eapli.base.net.SDP2021;
 import eapli.base.net.SDP2021Code;
 import eapli.base.net.motorflux.ActivityFlowClient;
 import eapli.base.taskmanagement.execution.domain.AutomaticTaskExecution;
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
 import eapli.base.taskmanagement.execution.domain.TaskExecution;
 import eapli.base.taskmanagement.execution.repositories.TaskExecutionRepository;
 import eapli.base.util.Application;
@@ -29,36 +30,51 @@ public class ActivityFlowController {
         return m_oFluxDataService.prepareFluxData(oUserName);
     }
 
-    public String advanceFluxData(Long fluxID) {
-        Optional<ActivityFluxExecution> afe = m_oAFERepo.findByID(fluxID);
-        if(afe.isPresent()) {
-            afe.get().advanceProgress();
-
-            Long idTask = afe.get().currentProgress().currentProgress();
-
-            if(idTask != -1) {
-                Optional<TaskExecution> oTe = m_oTERepo.findByID(idTask);
-                TaskExecution te;
-                if(!oTe.isPresent()) return "FAILURE";
-                te = oTe.get();
-                if(te.getClass().equals(AutomaticTaskExecution.class)) {
-                    AutomaticTaskExecution ate = (AutomaticTaskExecution) te;
-                    ate.setExecuting();
-                    ActivityFlowClient afc = new ActivityFlowClient(EXECUTE_SERVER_IP);
-                    afc.retrieveInformation(te.id() + "," +
-                            ate.getM_oAutomaticTask().script().toString(), SDP2021Code.AUTOTASK_REQUEST.getCode());
-                    afc.retrieveInformation("", SDP2021Code.END.getCode());
-                } else {
-
-                }
-            }
-
-
-
-
-            return "SUCCESS";
+    public String creationFluxData(Long fluxID) {
+        Optional<ActivityFluxExecution> oAfe = m_oAFERepo.findByID(fluxID);
+        if (oAfe.isPresent()) {
+            ActivityFluxExecution afe = oAfe.get();
+            return String.valueOf(setExecuting(afe).booleanValue());
         }
-        return "FAILURE";
+        return Boolean.FALSE.toString();
     }
 
+    public String advanceFluxData(Long fluxID) {
+        Optional<ActivityFluxExecution> oAfe = m_oAFERepo.findByID(fluxID);
+        if (oAfe.isPresent()) {
+            ActivityFluxExecution afe = oAfe.get();
+            afe.advanceProgress();
+            if (afe.currentProgress().currentProgress() == -1) return "Finished activity flux";
+            return String.valueOf(setExecuting(afe));
+        }
+        return "Invalid activity flux execution id";
+    }
+
+    private Boolean setExecuting(ActivityFluxExecution afe) {
+        Long idTask = afe.currentProgress().currentProgress();
+        if (idTask != -1) {
+            Optional<TaskExecution> oTe = m_oTERepo.findByID(idTask);
+            TaskExecution te;
+            if (!oTe.isPresent()) return false;
+            te = oTe.get();
+            te.setExecuting();
+            m_oAFERepo.save(afe);
+            m_oTERepo.save(te);
+            if (te.getClass().equals(AutomaticTaskExecution.class)) {
+                AutomaticTaskExecution ate = (AutomaticTaskExecution) te;
+                ActivityFlowClient afc = new ActivityFlowClient(EXECUTE_SERVER_IP);
+                afc.retrieveInformation(te.id() + "," +
+                        ate.getM_oAutomaticTask().script().toString(), SDP2021Code.AUTOTASK_REQUEST.getCode());
+                afc.retrieveInformation("", SDP2021Code.END.getCode());
+                advanceFluxData(afe.id());
+            } else {
+                if (te.getClass().equals(ManualTaskExecution.class)) {
+                    ManualTaskExecution mte = (ManualTaskExecution) te;
+                    //TODO: FINISH MANUAL TASK EXECUTION
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
