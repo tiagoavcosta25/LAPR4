be7d4d3ed26bfd3b3c74c6ee83043968d2e8e496
1190967 <1190967@isep.ipp.pt>
2021-06-19 23:53:53 +0100

    LAPR4-177 #review #fixing [US-0-3003] Unitary Testing Activity Flux

--- project/base.core/src/test/java/eapli/base/activityfluxmanagement/domain/ActivityFluxTest.java
+++ project/base.core/src/test/java/eapli/base/activityfluxmanagement/domain/ActivityFluxTest.java
@@ -12,14 +12,15 @@ import static org.junit.Assert.assertTrue;
 
 public class ActivityFluxTest {
 
-    /*@Test
+    @Test
     public void ensureCanBuildActivityFluxWithApprovalAndManualTask() {
-        final Form oApprovalForm = new Form(FormName.valueOf("Approval Form"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Approval"),
-                AttributeLabel.valueOf("Approval"), AttributeDescription.valueOf("Approval|Rejected"), AttributeRegex.valueOf("[0-9]+"),
-                FormScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final Form oApprovalForm = new Form(FormName.valueOf("Approval Form"), FormType.MANUALTASK, FormScript.valueOf("D:/folder3/script3.bat")
+                , new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Approval"),
+                AttributeLabel.valueOf("Approval"), AttributeDescription.valueOf("Approval|Rejected"),
+                AttributeRegex.valueOf("[0-9]+"), DataType.STRING))));
         final ManualTask oApprovalTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, oApprovalForm);
-        final Form oForm = new Form(FormName.valueOf("Form Name"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Attribute"),
-                AttributeLabel.valueOf("Label"), AttributeDescription.valueOf("Description"), AttributeRegex.valueOf("[0-9]+"), FormScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final Form oForm = new Form(FormName.valueOf("Form Name"), FormType.MANUALTASK, FormScript.valueOf("D:/folder3/script3.bat"), new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Attribute"),
+                AttributeLabel.valueOf("Label"), AttributeDescription.valueOf("Description"), AttributeRegex.valueOf("[0-9]+"), DataType.STRING))));
         final ManualTask oManualTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.LOW, oForm);
         final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oApprovalTask, oManualTask)));
         assertTrue(subject != null);
@@ -27,8 +28,8 @@ public class ActivityFluxTest {
 
     @Test
     public void ensureCanBuildActivityFluxWithOnlyManualTask() {
-        final Form oForm = new Form(FormName.valueOf("Form Name"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Attribute"),
-                AttributeLabel.valueOf("Label"), AttributeDescription.valueOf("Description"), AttributeRegex.valueOf("[0-9]+"), FormScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final Form oForm = new Form(FormName.valueOf("Form Name"), FormType.MANUALTASK, FormScript.valueOf("D:/folder3/script3.bat"), new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Attribute"),
+                AttributeLabel.valueOf("Label"), AttributeDescription.valueOf("Description"), AttributeRegex.valueOf("[0-9]+"), DataType.STRING))));
         final ManualTask oManualTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.LOW, oForm);
         final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oManualTask)));
         assertTrue(subject != null);
@@ -36,9 +37,10 @@ public class ActivityFluxTest {
 
     @Test
     public void ensureCanBuildActivityFluxWithApprovalAndAutoTask() {
-        final Form oApprovalForm = new Form(FormName.valueOf("Approval Form"), FormType.MANUALTASK, new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Approval"),
-                AttributeLabel.valueOf("Approval"), AttributeDescription.valueOf("Approval|Rejected"), AttributeRegex.valueOf("[0-9]+"),
-                FormScript.valueOf("D:/folder3/script3.bat"), DataType.STRING))));
+        final Form oApprovalForm = new Form(FormName.valueOf("Approval Form"), FormType.MANUALTASK, FormScript.valueOf("D:/folder3/script3.bat"),
+                new ArrayList<>(Arrays.asList(new Attribute(AttributeName.valueOf("Approval"),
+                AttributeLabel.valueOf("Approval"), AttributeDescription.valueOf("Approval|Rejected"),
+                        AttributeRegex.valueOf("[0-9]+"), DataType.STRING))));
         final ManualTask oApprovalTask = new ManualTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, oApprovalForm);
         final AutomaticTask oAutoTask = new AutomaticTask(TaskDescription.valueOf("Task Description"), TaskPriority.HIGH, AutomaticTaskScript.valueOf("D:/folder3/script3.bat"));
         final ActivityFlux subject = new ActivityFlux(new ArrayList<>(Arrays.asList(oApprovalTask, oAutoTask)));
@@ -56,6 +58,6 @@ public class ActivityFluxTest {
     public void ensureCantBuildActivityFluxWithoutTasks() {
         final ActivityFlux subject = new ActivityFlux(new ArrayList<>());
         assertTrue(subject != null);
-    }*/
+    }
 
 }
