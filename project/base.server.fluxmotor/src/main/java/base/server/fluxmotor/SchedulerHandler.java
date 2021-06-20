3125ac4f6b32999eb796f7c35d1e78584f1f8866
1190424 <1190424@isep.ipp.pt>
2021-06-20 13:45:02 +0100

    LAPR4-208 #implementation #developing [US-3-4072] SchedulerHandler

--- project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/SchedulerHandler.java
+++ project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/SchedulerHandler.java
@@ -1,4 +1,30 @@
 package base.server.fluxmotor;
 
-public class SchedulerHandler {
-}
+import eapli.base.collaboratormanagement.domain.Collaborator;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.taskmanagement.execution.domain.ManualTaskExecution;
+import eapli.base.taskmanagement.execution.repositories.ManualTaskExecutionRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
+
+/**
+ * @author Beatriz Seixas 1191460@isep.ipp.pt
+ */
+public class SchedulerHandler extends Thread{
+
+    private static final Logger LOGGER = LogManager.getLogger(SchedulerHandler.class);
+    private final SchedulerQueue schedulerQueue;
+    private final Collaborator collab;
+    private final ManualTaskExecutionRepository m_oMTERepo = PersistenceContext.repositories().manualTaskExec();
+    private final int nThread;
+
+    public SchedulerHandler(int num, SchedulerQueue schedulerQueue, Collaborator collab) {
+        this.collab = collab;
+        this.schedulerQueue = schedulerQueue;
+        this.nThread = num;
+    }
+
+
+    }
