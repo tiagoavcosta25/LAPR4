37551ef4765f12f5ef3b9c581ad68283f084059e
Tiago Costa <1191460@isep.ipp.pt>
2021-06-20 13:31:18 +0100

    LAPR4-207 #implementation #FCFS [US-3-4072] AlgorithmMode.java implementation

---
+++ project/base.server.fluxmotor/src/main/java/base/server/fluxmotor/AlgorithmMode.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates and open the template
+ * in the editor.
+ */
+package base.server.fluxmotor;
+
+/**
+ * @author Jéssica Alves 1190682@isep.ipp.pt
+ * @author Pedro Santos 1190967@isep.ipp.pt
+ */
+public enum AlgorithmMode {
+    /**
+     * Modes.
+     */
+    FCFS(0),
+    SCHEDULER(1);
+
+    /**
+     * Mode.
+     */
+    private int m_intMode;
+
+    /**
+     * An empty constructor of ScriptMode.
+     * @param intMode Mode.
+     */
+    AlgorithmMode(int intMode) {
+        this.m_intMode = intMode;
+    }
+
+    public int getMode() {
+        return m_intMode;
+    }
+
+    public boolean equals(AlgorithmMode oMode){
+        return this.getMode() == oMode.getMode();
+    }
+
+    public static AlgorithmMode getMode(Integer intMode) {
+        if(intMode == AlgorithmMode.FCFS.getMode()){
+            return AlgorithmMode.FCFS;
+        } else if(intMode == AlgorithmMode.SCHEDULER.getMode()){
+            return AlgorithmMode.SCHEDULER;
+        }
+        return null;
+    }
+}
